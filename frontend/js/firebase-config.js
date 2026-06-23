a3451475f1c7849b33ba9167ae1e34ea3e53b0e3
threenine0831 <daekyu1112@gmail.com>
2026-06-23 15:04:50 +0900

    Include Firebase web config for frontend deploy

---
+++ frontend/js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC-CVhKmjLR3s6oZCiXUt2adwQMIOkNKxk",
+  authDomain: "webproject-226fa.firebaseapp.com",
+  projectId: "webproject-226fa",
+  storageBucket: "webproject-226fa.firebasestorage.app",
+  messagingSenderId: "531417677364",
+  appId: "1:531417677364:web:5d7fa3da6a1b05685fd461",
+  measurementId: "G-5VM6B9BMNN"
+};
